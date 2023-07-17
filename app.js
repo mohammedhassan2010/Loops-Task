6d4b8c8c682d87e45b759ca4d376aa45ffaa6c0f
Mohammedhassan <mohammedhassann@outlook.com>
2023-07-17 21:12:43 +0300

    I sloved the task

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-function calculateFactorial(number) {}
+function calculateFactorial(number) {
+    let factorial = 1;
+   
+    for (let i = 1; i<=number; i++) {
+        factorial = factorial * i;
+    };
+    return factorial;
+}
+const number = 5;
+const result = calculateFactorial(number);
+console.log(`The factorial of ${number} is ${result}`);
+
+
+
+  
